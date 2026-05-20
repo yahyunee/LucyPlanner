eaed93d29a4b140dc60a72642fe6c8752db162f5
yahyunee <ahhyun724@snu.ac.kr>
2026-05-20 12:04:49 -0400

    Initial Commit

---
+++ LucyPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LucyPlanner
+//
+//  Created by lucy on 5/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LucyPlanner/LucyPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucyPlannerApp.swift
+//  LucyPlanner
+//
+//  Created by lucy on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct LucyPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
